60c1b9fbdc46d84113dbe51736869763c135dbee
sct213 <yang991211@naver.com>
2021-01-15 08:29:27 +0900

    Swift

--- Exam.playground/Pages/Untitled Page 2.xcplaygroundpage/Contents.swift
+++ Exam.playground/Pages/Untitled Page 2.xcplaygroundpage/Contents.swift
@@ -1,7 +1,14 @@
-//: [Previous](@previous)
-
 import Foundation
 
-var str = "Hello, playground"
+var tset: Int = (Int.random(in: -100...100))
 
-//: [Next](@next)
+typealias MyInt = Int
+typealias YourInt = Int
+typealias MyDouble = Double
+
+let age: MyInt = 100
+var year: YourInt = 2080
+
+var person: (name: String,age: Int,height: Double) = ("yagom", 100, 182.5)
+
+print("이름: \(person.name), 나이: \(person.age), 키: \(person.height)")
