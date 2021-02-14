3f802c475fbfff57ad1f2525adb6248612d647d0
sct213 <yang991211@naver.com>
2021-02-14 09:34:00 +0900

    Swift

--- Mastering_Swift/18-Methods And Subscripts/IntanceMethods.playground/Pages/Syntax.xcplaygroundpage/Contents.swift
+++ Mastering_Swift/18-Methods And Subscripts/IntanceMethods.playground/Pages/Syntax.xcplaygroundpage/Contents.swift
@@ -1,48 +1,58 @@
-//
-//  Copyright (c) 2018 KxCoding <kky0317@gmail.com>
-//
-//  Permission is hereby granted, free of charge, to any person obtaining a copy
-//  of this software and associated documentation files (the "Software"), to deal
-//  in the Software without restriction, including without limitation the rights
-//  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-//  copies of the Software, and to permit persons to whom the Software is
-//  furnished to do so, subject to the following conditions:
-//
-//  The above copyright notice and this permission notice shall be included in
-//  all copies or substantial portions of the Software.
-//
-//  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-//  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-//  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-//  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-//  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-//  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
-//  THE SOFTWARE.
-//
 import UIKit
-
 /*:
  # Instance Method
  ![Method](method.png)
  ![Call Method](call-method.png)
  */
 
+class Sample {
+    var data = 0
+    static var sharedData = 123
+    
+    func doSomething() {
+        print(data)
+        // 형식을 붙여야 접근 가능
+        Sample.sharedData
+    }
+    func call() {
+        doSomething()
+    }
+}
+
+let a = Sample()
+a.data
+a.doSomething()
+
+class Size {
+    var width = 0.0
+    var height = 0.0
+    
+    func enlarge() {
+        width += 1.0
+        height += 1.0
+    }
+}
+
+let s = Size()
+s.enlarge()
 
 
+struct Size1 {
+    var width = 0.0
+    var height = 0.0
+    
+    mutating func enlarge() {
+        width += 1.0
+        height += 1.0
+    }
+}
 
+// 값형식에서 속성을 변경할 때는 mutating을 붙여야 함
 
+// 변수로 선언하였기에 값을 변경할 수 있지만, 
+var s1 = Size1()
 
-
-
-
-
-
-
-
-
-
-
-
+s1.enlarge()
 
 
 
--- Mastering_Swift/18-Methods And Subscripts/Subscripts.playground/Pages/Syntax.xcplaygroundpage/Contents.swift
+++ Mastering_Swift/18-Methods And Subscripts/Subscripts.playground/Pages/Syntax.xcplaygroundpage/Contents.swift
@@ -1,45 +1,92 @@
-//
-//  Copyright (c) 2018 KxCoding <kky0317@gmail.com>
-//
-//  Permission is hereby granted, free of charge, to any person obtaining a copy
-//  of this software and associated documentation files (the "Software"), to deal
-//  in the Software without restriction, including without limitation the rights
-//  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-//  copies of the Software, and to permit persons to whom the Software is
-//  furnished to do so, subject to the following conditions:
-//
-//  The above copyright notice and this permission notice shall be included in
-//  all copies or substantial portions of the Software.
-//
-//  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-//  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-//  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-//  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-//  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-//  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
-//  THE SOFTWARE.
-//
-
 import UIKit
-
 /*:
  # Subscripts
  ![subscript](subscript.png)
  */
-
-
-
-
-
-
-
-
-
-
-
+let list = ["A", "B", "C"]
+// 이러한 형태가 Subscript
+list[0]
 /*:
  ![subscript-syntax](subscript-syntax.png)
  */
+class List {
+    var data = [1, 2, 3]
+    
+    subscript(i index: Int) -> Int {
+        get {
+            return data[index]
+        }
+        
+        set {
+            data[index] = newValue
+        }
+    }
+}
+
+var li = List()
+// 값을 가져올때 get 실행
+li[i: 0]
+// 값을 수정할때 set 실행
+li[i: 1] = 123
+
+// Argument Label. 하지만 잘 사용하지 않는다.
+li[i: 1]
+
+struct Matrix {
+    var data = [[1, 2, 3],
+                [4, 5, 6],
+                [7, 8, 9]]
+    
+    subscript(row: Int, col: Int) -> Int {
+        return data[row][col]
+    }
+}
+
+var Ma = Matrix()
+
+// subscript로 배열을 전달할 경우.
+Ma[0, 1]
+
+//MARK: - Dynamic Member Lookup
+
+// 코드의 유연성이 높아짐.
+@dynamicMemberLookup
+struct Person {
+    var name: String
+    var address: String
+    
+    subscript(dynamicMember member: String) -> String {
+        switch member {
+            case "nameKey":
+                return name
+            case "addressKey":
+                return address
+            default:
+                return "N/A"
+        }
+    }
+}
+
+let p = Person(name: "James", address: "Seoul")
+p.name
+p.address
+
+p[dynamicMember: "nameKey"]
+p[dynamicMember: "addressKey"]
+
+p.nameKey
+p.addressKey
+
+p.missingKey
+
+
+
+
+
+
+
+
+
 
 
 
