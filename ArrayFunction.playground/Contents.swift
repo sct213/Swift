bbd1d88e0af55b8dfbb952994ea54fa96c0c554b
sct213 <yang991211@naver.com>
2021-03-05 14:49:08 +0900

    Array & Algorithm Start

--- ArrayFunction.playground/Contents.swift
+++ ArrayFunction.playground/Contents.swift
@@ -66,6 +66,68 @@ print(arr)
 arr.insert(5, at: 4)
 arr.insert(0, at: 0)
 
+// 해당 인덱스 삭제
+arr.remove(at: 0)
+
+arr.removeAll()
+
+// 4. 첫번째 값을 제거하고 싶으면 removeFirst()
+//arr.removeFirst()
+
+// 5. 마지막 값을 제거하고 싶으면 popLast(), removeLast()
+arr.popLast()
+// 값이 없는 nil상태여도, nil값을 pop하기 때문에 오류가 아님
+
+// 만약 값이 없다면 오류 발생
+//arr.removeLast()
+
+// 첫번째 값 또는 마지막 값이 없는 배열을 리턴. dropFirst(), dropLast()
+arr = [1,2,3,4,5]
+arr.dropFirst()
+arr.dropLast()
+print(arr)
+
+
+/* 배열 값 접근 */
+
+// 1. startIndex
+arr = [60, 70, 80, 90, 100]
+
+arr.startIndex
+print(arr[arr.startIndex])
+
+// 2. endIndex
+arr.endIndex
+//print(arr[arr.endIndex])
+
+// 3. first/Last
+// 인덱스와 다르게 값에 바로 접근할 수 있음
+arr.first
+arr.last
+
+// 4. 배열안에서 최소값, 최대값 얻기 min()/max()
+arr.min()
+arr.max()
+
+
+/* 배열 변경 */
+arr = [40, 10, 20, 80, 100]
+
+// 1. 오름차순
+arr.sort()
+// 2. 내림차순
+arr.reverse()
+
+// 3. 배열 오름차순 정렬(배열리턴) sorted()
+// sort()는 원본 배열 자체를 아예 정렬해버리지만,
+// sorted()는 그렇지 않다.
+arr.sorted()
+
+// 4. 배열 내림차순 정렬
+arr.sorted(by: >)
+
+
+
 
 
 
